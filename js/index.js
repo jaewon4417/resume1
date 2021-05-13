3e26ebffb6ece1766b18c4ca9def9db9cae9f82c
jaewon4417 <77268907+jaewon4417@users.noreply.github.com>
2021-05-13 23:46:14 +0900

    v.2

--- js/index.js
+++ js/index.js
@@ -28,20 +28,7 @@ $(document).ready(function(){
     // })
 })
 // 마우스 
-// http://ahrengot.com/tutorials/greensock-javascript-animation
 
-var $circle = $('.circle');
-
-function moveCircle(e) {
-  TweenLite.to($circle, 0.3, {
-    css: {
-      left: e.pageX,
-      top: e.pageY
-    }
-  });
-}
-
-$(window).on('mousemove', moveCircle);
 
 // about me button
 function onButtonClick() {
@@ -53,6 +40,7 @@ function onButtonClick() {
         // 여기만 바꾸면 돼
         backgroundColor : "#F2E1CD"
     })
+    $('.clickme').addClass("blind")
 }
 // about me button
 function offButtonClick() {
@@ -64,6 +52,7 @@ function offButtonClick() {
         // 여기만 바꾸면 돼
         backgroundColor : "#4A4A4A"
     })
+    $('.clickme').removeClass("blind")
 }
 // introduce 애니메이션
 
@@ -92,3 +81,93 @@ function closeWebDesignBox() {
 function openWebDesignBox() {
     $('.webdesign_bg').removeClass("blind")
 }
+// 고양이
+function init() { 
+    const catWrapper = document.querySelector('.cat_wrapper')
+    const wrapper = document.querySelector('.wrapper')
+    const cat = document.querySelector('.cat')
+    const head = document.querySelector('.cat_head')
+    const legs = document.querySelectorAll('.leg')
+    const pos = {
+      x: null,
+      y: null
+    }
+  
+    const walk = () =>{
+      cat.classList.remove('first_pose')
+      legs.forEach(leg=>leg.classList.add('walk'))
+    }
+  
+    const handleMouseMotion = e =>{
+      pos.x = e.clientX
+      pos.y = e.clientY
+      walk()
+    }
+  
+    const handleTouchMotion = e =>{
+      if (e.targetTouches) return
+      
+      pos.x = e.targetTouches[0].offsetX
+      pos.y = e.targetTouches[0].offsetY
+      walk()
+    }
+  
+    const turnRight = () =>{
+      cat.style.left = `${pos.x - 90}px`
+      cat.classList.remove('face_left')
+      cat.classList.add('face_right')
+    }
+  
+    const turnLeft = () =>{
+      cat.style.left = `${pos.x + 10}px`
+      cat.classList.remove('face_right')
+      cat.classList.add('face_left')
+    }
+  
+    const decideTurnDirection = () =>{
+      cat.getBoundingClientRect().x < pos.x ?
+        turnRight()
+        :
+        turnLeft()
+    }
+  
+    const headMotion = () =>{
+      pos.y > (wrapper.clientHeight - 100) ?
+        head.style.top = '-15px'
+        :
+        head.style.top = '-30px'
+    }
+  
+    const jump = () =>{
+      catWrapper.classList.remove('jump')
+      if (pos.y < (wrapper.clientHeight - 250)) {
+        setTimeout(()=>{
+          catWrapper.classList.add('jump')
+        },100)
+      } 
+    }
+  
+    const decideStop = ()=>{
+      if (cat.classList.contains('face_right') && pos.x - 90 === cat.offsetLeft ||
+          cat.classList.contains('face_left') && pos.x + 10 === cat.offsetLeft) {
+        legs.forEach(leg=>leg.classList.remove('walk'))    
+      }
+    }
+    
+    setInterval(()=>{
+      if (!pos.x || !pos.y) return
+      decideTurnDirection()
+      headMotion()
+      decideStop()
+    },100)
+  
+    setInterval(()=>{
+      if (!pos.x || !pos.y) return
+      jump()
+    },1000)
+  
+    document.addEventListener('mousemove', handleMouseMotion)
+    document.addEventListener('mousemove', handleTouchMotion)
+  }
+  
+  window.addEventListener('DOMContentLoaded', init)
